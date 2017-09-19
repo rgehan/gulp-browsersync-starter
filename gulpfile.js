52ce93d2f953220feb4aca5769e2c2f5c9cd02ed
Renan GEHAN <rgehan94@gmail.com>
2017-09-19 19:23:02 +0200

    Add webpack for js transpilation

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,25 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const concat = require('gulp-concat');
+const rename = require('gulp-rename');
 const browserSync = require('browser-sync').create();
+const webpack = require('gulp-webpack');
 
 const paths = {
   sass: 'sass/**/*.scss',
+  js: 'js/**/*.js',
   html: 'public/**/*.html',
-  css: './public/',
+  out: './public/',
 };
 
 // Serve the app
-gulp.task('serve', ['sass'], () => {
+gulp.task('serve', ['sass', 'js'], () => {
   browserSync.init({
     server: './public',
   });
 
   gulp.watch(paths.sass, ['sass']);
+  gulp.watch(paths.js, ['js']);
   gulp.watch(paths.html).on('change', browserSync.reload);
 });
 
@@ -24,7 +28,15 @@ gulp.task('sass', () => {
   return gulp.src(paths.sass)
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('index.css'))
-    .pipe(gulp.dest(paths.css))
+    .pipe(gulp.dest(paths.out))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('js', () => {
+  return gulp.src('js/index.js')
+    .pipe(webpack())
+    .pipe(rename('index.js'))
+    .pipe(gulp.dest(paths.out))
     .pipe(browserSync.stream());
 });
 
